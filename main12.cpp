5b54e93b79f17b7598287714f08af49b1fe7e108
Shahd Mostafa <164599571+Shahd3711@users.noreply.github.com>
2025-12-09 17:14:44 +0200

    Update main12.cpp

--- main12.cpp
+++ main12.cpp
@@ -3,42 +3,38 @@
 #include <stdexcept>
 #include <limits>
 using namespace std;
-int read_list_size() {
-    int size = 0;
-    while (true) {
-        cout << "Enter the size of the list you want to create: ";
-        if (cin >> size && size >= 0) {
+int read_sz() 
+{
+    int sz=0;
+    while(1) 
+    {
+        cout<<"Enter the size of the list: ";
+        if(cin>>sz&&sz>=0) 
+        {
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-            return size;
-        } else {
-            cout << "Invalid input. Please enter a non-negative integer." << endl;
+            return sz;
+        }
+        else
+        {
+            cout<<"Invalid input! Please enter a non-negative integer."<<endl;
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     }
 }
-
-int main() {
-    cout << "### Dynamic Type List Generator (C++17) ###" << endl;
-
-    int list_size = read_list_size();
-    
+int main() 
+{
+    int sz=read_sz();
     DynamicList my_dynamic_list;
-    my_dynamic_list.reserve(list_size);
-
-    cout << "\nStarting input process for " << list_size << " items..." << endl;
-
-    for (int i = 0; i < list_size; ++i) {
-        try {
-            process_user_input(my_dynamic_list, i);
-        } catch (const exception& e) {
-            cerr << "An unexpected error occurred: " << e.what() << endl;
-            i--; 
-        }
-    }
-
-    display_list_content(my_dynamic_list);
+    my_dynamic_list.reserve(sz);
+    cout << "\n Gr8! Just a sec..." << sz << " Loading..." << endl;
+    for(int i=0; i<sz; i++)
+    {
+        check(my_dynamic_list, i);
+        //cout<<typeid(my_dynamic_list[i]).name()<<endl;
+    } 
+    display(my_dynamic_list);
 
     return 0;
 }
-//g++ -std=c++17 main.cpp list.cpp -o main12
+//g++ -std=c++17 main12.cpp list.cpp -o main12
